0351a8b6abcc557dc3c83bc18069f998e8404edf
James Newton <blackslate@lexogram.com>
2023-11-17 13:35:48 +0300

    Fix failure to display video info
    Edit VideoSearchComponent.js
    ---
    * Get info from video.snippet
    * Changed one-line map to a function that returns a div manually
    * Use deconstruction to get data
    * Get imageURL from info.thumbnails.default.url

--- src/youtube/VideoSearchComponent.js
+++ src/youtube/VideoSearchComponent.js
@@ -27,13 +27,22 @@ const VideoSearchComponent = () => {
             </button>
             {videos.length > 0 ? (
                 <div className="mt-4">
-                    {videos.map(video => (
-                        <div key={video.id} className="mb-4 p-2 border border-red-300 rounded-md">
-                            <h3 className="text-lg text-red-700">{video.title}</h3>
-                            <img src={video.thumbnails} alt={video.title} className="mt-2" />
+                    {videos.map(video => {
+                        // console.log("video:", video);
+                        
+                        const id = video.id.videoId
+                        const info = video.snippet
+                        const { thumbnails, title } = info
+                        const imageURL = thumbnails.default.url
+
+                        return (
+                        <div key={id} className="mb-4 p-2 border border-red-300 rounded-md">
+                            <h3 className="text-lg text-red-700">{title}</h3>
+                            <img src={imageURL} alt={title} className="mt-2" />
                             {/* You can also include other video details like description, channel, etc. */}
                         </div>
-                    ))}
+                        )}
+                    )}
                 </div>
             ) : (
                 <p className="mt-4 text-red-700">No videos found. Try a different search.</p>
